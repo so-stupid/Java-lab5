caedce18e1eb526b30e4ce9add14664b53ddd065
so-stupid <evankey2k17@yandex.ru>
2020-12-03 04:04:15 +0300

    Add footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { LargeCardComponent } from './large-card/large-card.component';
 import {RouterModule} from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-    declarations: [MenuComponent, LargeCardComponent],
-  exports: [
-    MenuComponent,
-    LargeCardComponent
-  ],
+    declarations: [MenuComponent, LargeCardComponent, FooterComponent],
+    exports: [
+        MenuComponent,
+        LargeCardComponent,
+        FooterComponent
+    ],
   imports: [
     CommonModule,
     RouterModule
